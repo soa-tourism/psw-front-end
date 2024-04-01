37895c019703b8f0831d4d9da241763be0904659
nina <ninabu16@gmail.com>
2024-04-01 18:57:48 +0200

    feat: change https to http

--- Explorer/src/env/environment.ts
+++ Explorer/src/env/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiHost: 'https://localhost:44333/api/',
-    wwwRoot: 'https://localhost:44333',
+    apiHost: 'http://localhost:44333/api/',
+    wwwRoot: 'http://localhost:44333',
     tours: 'http://localhost:8081/v1',
 };
   
